20dc15d8b1b7eef5bc7f45af8f09aee0961adca3
praveen005 <praveen08h@gmail.com>
2020-08-17 17:54:10 +0530

    comment out 4 lines

--- CB/recursion/2048spelling.cpp
+++ CB/recursion/2048spelling.cpp
@@ -18,10 +18,12 @@ void printSpelling(int n) {
 	cout << ch[digit] << " ";
 }
 int main() {
+/*
 #ifndef ONLINE_JUDGE
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
 #endif
+*/
 	int n; cin >> n;
 	printSpelling(n);
 }
