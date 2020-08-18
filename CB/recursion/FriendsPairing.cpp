923b925ea022adf083d44f86eadae31a82e44844
praveen005 <praveen08h@gmail.com>
2020-08-18 21:13:37 +0530

    Added a file

---
+++ CB/recursion/FriendsPairing.cpp
@@ -0,0 +1,33 @@
+/*
+	problem: Given N friends,who want to go for outing on bikes. Each guy can go as single or as a couple.
+		find the number of ways in which N friends can go for outing.
+		
+		note: They have enough bikes for solo trip.
+
+*/
+
+
+
+#include<iostream>
+#include<cstring>
+using namespace std;
+int GoForOuting(int n) {
+//base case
+	if (n == 0 || n == 1) {
+		return 1;
+	}
+	int ans;
+//recursive case
+	ans = GoForOuting(n - 1) + (n - 1) * GoForOuting(n - 2);
+	return ans;
+}
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	int n;
+	cin >> n;
+	cout << GoForOuting(n) << endl;
+
+}
